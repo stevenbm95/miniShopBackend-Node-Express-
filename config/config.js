2086c83fd20601f9806c00620d4c81a7f929c835
stevenbm95 <stevenbm95@gmail.com>
2024-11-12 20:13:19 -0500

    Vercel Info

--- config/config.js
+++ config/config.js
@@ -1,29 +1,77 @@
 require("dotenv").config();
 
 
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USER || "root",
+//     password: process.env.DB_PASSWORD || "root",
+//     database: process.env.DB_NAME || "minishop",
+//     host: process.env.DB_HOST || "localhost",
+//     dialect: "mysql",
+//     port: process.env.DB_PORT || 3306,
+//   },
+//   test: {
+//     username: process.env.DB_USER || "root",
+//     password: process.env.DB_PASSWORD || "root",
+//     database: process.env.DB_NAME || "minishop",
+//     host: process.env.DB_HOST || "localhost",
+//     dialect: "mysql",
+//     port: process.env.DB_PORT || 3306,
+//   },
+//   production: {
+//     username: process.env.DB_USER || "root",
+//     password: process.env.DB_PASSWORD || "root",
+//     database: process.env.DB_NAME || "minishop",
+//     host: process.env.DB_HOST || "localhost",
+//     dialect: "mysql",
+//     port: process.env.DB_PORT || 3306,
+//   },
+// };
+
 module.exports = {
   development: {
-    username: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "root",
-    database: process.env.DB_NAME || "minishop",
-    host: process.env.DB_HOST || "localhost",
-    dialect: "mysql",
-    port: process.env.DB_PORT || 3306,
+    username: process.env.DB_USER || "default",
+    password: process.env.DB_PASSWORD || "KrLC4ofiOj3m",
+    database: process.env.DB_NAME || "verceldb",
+    host: process.env.DB_HOST || "ep-polished-river-a4k46f5i.us-east-1.aws.neon.tech",
+    dialect: "postgres",
+    port: process.env.DB_PORT || 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Para evitar problemas con SSL en conexiones remotas.
+      },
+    },
   },
   test: {
-    username: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "root",
-    database: process.env.DB_NAME || "minishop",
-    host: process.env.DB_HOST || "localhost",
-    dialect: "mysql",
-    port: process.env.DB_PORT || 3306,
+    username: process.env.DB_USER || "default",
+    password: process.env.DB_PASSWORD || "KrLC4ofiOj3m",
+    database: process.env.DB_NAME || "verceldb",
+    host: process.env.DB_HOST || "ep-polished-river-a4k46f5i.us-east-1.aws.neon.tech",
+    dialect: "postgres",
+    port: process.env.DB_PORT || 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
-    username: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "root",
-    database: process.env.DB_NAME || "minishop",
-    host: process.env.DB_HOST || "localhost",
-    dialect: "mysql",
-    port: process.env.DB_PORT || 3306,
+    username: process.env.DB_USER || "default",
+    password: process.env.DB_PASSWORD || "KrLC4ofiOj3m",
+    database: process.env.DB_NAME || "verceldb",
+    host: process.env.DB_HOST || "ep-polished-river-a4k46f5i.us-east-1.aws.neon.tech",
+    dialect: "postgres",
+    port: process.env.DB_PORT || 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
+
+
+
